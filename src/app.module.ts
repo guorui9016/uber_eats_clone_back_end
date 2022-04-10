47276ec5e5296ca554db1af6d9288c5c8013ea2f
Ray <guorui9016@gmail.com>
2022-04-10 23:30:18 +1000

    initial the project

---
+++ src/app.module.ts
@@ -0,0 +1,45 @@
+import { ApolloDriver } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    // GraphQLModule.forRoot({
+    //   driver: ApolloDriver,
+    //   autoSchemaFile:true
+    // }),
+
+    ConfigModule.forRoot({
+      isGlobal:true,
+      envFilePath: process.env.NODE_ENV==='dev' ? '.env.dev' : '.env.test',
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin',
+      database: 'nuber_eats_2',
+      // host: process.env.DB_HOST,
+      // port: +process.env.DB_PORT,
+      // username: process.env.DB_USER_NAME,
+      // password: process.env.DB_PASSWORD,
+      // database: process.env.DB_DATABASE,
+      entities:  ["dist/**/*.entity.js"],
+      // entities: [Account, Verification,],
+      synchronize: true,
+      logging: true
+    }),
+    
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {
+  constructor(){
+    console.log(process.env.DB_USER_NAME,process.env.DB_PASSWORD,)
+  }
+}
